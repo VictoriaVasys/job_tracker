a325029065d4ad8aad32a012a527b59419186130
caward12 <ward.colleen.a@gmail.com>
2017-03-30 21:37:07 -0600

    style categories jobs companies

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe "User deletes existing job" do
   scenario "a user can delete a job" do
     company = Company.create(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    category = Category.create(title: "Engineering")
+
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
 
     visit company_job_path(company.id, job.id)
 
